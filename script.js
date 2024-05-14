84294102c817d6576d5f8fc6cc26806ec9a4fc27
Supakarn Tangsirivatthanavong <hikaru8889@gmail.com>
2024-05-14 13:54:01 +0700

    finish exercise

---
+++ script.js
@@ -0,0 +1,25 @@
+// Exercise 1.1 Create function add, subtract, multiply, divide
+function add(a, b) {
+	return a + b;
+}
+function subtract(a, b) {
+	return a - b;
+}
+function multiply(a, b) {
+	return a * b;
+}
+function divide(a, b) {
+	return a / b;
+}
+
+// Exercise 1.2 Create Function Inform
+function info(firstName, location, hobby) {
+	console.log(
+		"Hi, my name is " +
+			firstName +
+			"I live in " +
+			location +
+			"and enjoy " +
+			hobby
+	);
+}
